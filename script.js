6f041cca84f2ad7a0656ba205169d15c4942dff8
david iwajomo <davidoiwajomo@gmail.com>
2021-10-19 19:07:38 +0100

    david

---
+++ script.js
@@ -0,0 +1,24 @@
+#
+fetch('https://programmingnewsapi.herokuapp.com/?format=json').then((data)=> {
+    return data.json();
+
+}).then((completedata)=>{
+    let data1="";
+    completedata.map((values)=>{
+        data1+= `  <div class="card" style="background-color:white;">
+
+        <h1 class="head" style="color:black;" >${values.title
+        }</h1>
+      
+  <img src=${values.image} alt="img"  class="images">
+  <p>${values.description}</p>
+      <button>
+    <a href=${values.link}>LINK</a>
+      </button>  
+        </div>`
+    
+    });
+    document.getElementById("cards").innerHTML=data1;
+}).catch((err)=>{
+    console.log(err);
+})
